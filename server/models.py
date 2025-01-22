b226a6dc75b3970a6d8271d1a7b42eead8638173
joyce wamocho <joywamo@gmail.com>
2025-01-22 20:32:25 +0300

    flask sqlalchemy validations

--- server/models.py
+++ server/models.py
@@ -12,6 +12,22 @@ class Author(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators 
+    @validates('name')
+    def validate_name(self,key,value):
+        if not value:
+            raise ValueError("Author must have a name")
+        
+        existing_author = Author.query.filter_by(name=value).first()
+        if existing_author:
+            raise ValueError(f"Author name '{value}' must be unique.")
+        return value
+
+    @validates('phone_number')
+    def validate_phone_number(self,key,value):
+        if len(value) != 10 or not value.isdigit():
+            raise ValueError('phone number must be exactly 10 digits')
+        return value
+
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
@@ -28,6 +44,37 @@ class Post(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators  
+
+    @validates('content')
+    def validate_content(self,key,value):
+        if len(value) < 250:
+            raise ValueError("content must atleast be 250 character long ")
+
+        return value 
+    
+    @validates('summary')
+    def validate_summary(self,key,value):
+        if len(value) >250:
+            raise ValueError("summart should have 250 characters maximum")
+        return value
+    @validates('category')
+    def validate_category(self,key,value):
+        if value not in ["Fiction","Non-Fiction"]:
+            raise ValueError('category must either be Fiction or Non-Fiction')
+        return value
+
+    @validates('title')
+    def validate_title(self, key, value):
+        clickbait_phrases = ["Won't Believe", "Secret", "Top", "Guess"]
+        if not any(phrase in value for phrase in clickbait_phrases):
+            raise ValueError(
+                "Title must contain at least one clickbait phrase: "
+                "'Won't Believe', 'Secret', 'Top', or 'Guess'."
+            )
+        return value
+
+
+    
 
 
     def __repr__(self):
